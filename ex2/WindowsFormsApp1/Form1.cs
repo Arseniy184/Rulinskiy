b24599b615bf0570391e1f56003308987243b307
Arseniy184 <74099186+Arseniy184@users.noreply.github.com>
2021-07-18 01:18:35 +0300

    ex2
    ex2

---
+++ ex2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static String resultSearch(String name)
+        {
+            String result = name.ToLower();
+            if (Equals(result, "вячеслав"))
+            {
+                result = "Привет, Вячеслав";
+            }
+            else
+            {
+                result = "Нет такого имени";
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            try
+            {
+                textBox2.Text = resultSearch(name);
+            }
+            catch (Exception ex)
+            {
+                textBox2.Text = $"Исключение: {ex.Message}";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
